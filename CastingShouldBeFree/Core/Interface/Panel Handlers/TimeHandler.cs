2565459ec229fd654ddb28627b9cbb94476d37a5
HanSolo1000Falcon <HanSolo1000Falcon.business@gmail.com>
2025-11-03 20:58:49 +0100

    working on sttuffs

---
+++ CastingShouldBeFree/Core/Interface/Panel Handlers/TimeHandler.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using UnityEngine.UI;
+
+namespace CastingShouldBeFree.Core.Interface.Panel_Handlers;
+
+public class TimeHandler : PanelHandlerBase
+{
+    protected override void Start()
+    {
+        transform.GetChild(2).GetComponent<Button>().onClick.AddListener(() =>
+                                                                         {
+                                                                             for (int i = 0;
+                                                                                  i < BetterDayNightManager.instance
+                                                                                         .weatherCycle.Length;
+                                                                                  i++)
+                                                                                 BetterDayNightManager.instance
+                                                                                                .weatherCycle[i] =
+                                                                                         BetterDayNightManager
+                                                                                                .WeatherType
+                                                                                                .Raining;
+                                                                         });
+        
+        transform.GetChild(3).GetComponent<Button>().onClick.AddListener(() =>
+                                                                         {
+                                                                             for (int i = 0;
+                                                                                  i < BetterDayNightManager.instance
+                                                                                         .weatherCycle.Length;
+                                                                                  i++)
+                                                                                 BetterDayNightManager.instance
+                                                                                                .weatherCycle[i] =
+                                                                                         BetterDayNightManager
+                                                                                                .WeatherType
+                                                                                                .None;
+                                                                         });
+
+        transform.GetChild(1).GetComponentInChildren<Slider>().maxValue = BetterDayNightManager.instance.timeOfDayRange.Length;
+        transform.GetChild(1).GetComponentInChildren<Slider>().onValueChanged.AddListener(value =>
+            {
+                int valueReal = (int)value;
+                BetterDayNightManager.instance.SetTimeOfDay(valueReal);
+                transform.GetChild(1).GetComponentInChildren<TextMeshProUGUI>().text = $"Current Time: {valueReal}";
+            });
+
+        base.Start();
+    }
+}
